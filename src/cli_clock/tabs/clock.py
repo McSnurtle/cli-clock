732290a30a9326781d251e8d13a9eed6a0c18c6b
McSnurtle <joepro17@outlook.com>
2025-11-01 14:01:41 -0400

    Add tab management
    - Create tab_registry.py to iteratively read and manage tab info from tabs/
    
    - Create tabs/ to store tabs and base.py Tabs baseclass

---
+++ src/cli_clock/tabs/clock.py
@@ -0,0 +1,41 @@
+# imports
+import datetime
+from typing import Any
+from .base import Tab
+
+try:
+    from ..utils.ascii_helper import generate_ascii, get_longest, INITIAL_X_OFFSET
+except ImportError:
+    from src.cli_clock.utils.ascii_helper import generate_ascii, get_longest, INITIAL_X_OFFSET
+
+# ===== Variables =====
+__bind__ = "c"
+__name__ = "clock"
+
+
+def get_time(fmt: str) -> str:
+    """Returns the formatted string representing the current system time."""
+    return datetime.datetime.now().strftime(fmt)
+
+
+class ClockTab(Tab):
+    keybind = "c"
+    name = "clock"
+
+    def draw(self, stdscr, height: int, width: int) -> None:
+        time_text: tuple[str] = generate_ascii(get_time(self.config["format"]), self.config["font"])
+
+        for idx, line in enumerate(time_text):
+            x_offset = INITIAL_X_OFFSET(self.config["font"])
+            if self.config["should_update_offset"]:
+                x_offset = get_longest(time_text)
+            stdscr.addstr(
+                int(height * 0.5 - (len(time_text) * 0.5 - idx + 1)),
+                int(width * 0.5 - x_offset * 0.5),
+                line)
+        if self.config["show_date"]:
+            date_text: str = f"{self.config['date_prefix']}{get_time(self.config['date_format'])}{self.config['date_suffix']}"
+            stdscr.addstr(
+                int((height * 0.5 - len(time_text) * 0.5 - 2)),
+                int((width * 0.5 - len(date_text) * 0.5)),
+                date_text)
